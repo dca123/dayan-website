850d88d09ec1454cdc488b0e021787968587b6e1
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-04-20 19:59:26 +0800

    latex

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   // used to generate images
   site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
+    process.env.VERCEL_ENV === "production"
+      ? "https://brutal.elian.codes/"
       : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
-  trailingSlash: 'ignore',
+        ? `https://${process.env.VERCEL_URL}/`
+        : "https://localhost:3000/",
+  trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
+      exclude: ["@resvg/resvg-js"],
     },
   },
 });
