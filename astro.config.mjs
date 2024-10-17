8fee7057e20d0ae28897e16a0a1acc1e22ca7f6b
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-10-17 18:07:06 +0530

    feat: Add blogposts
    * added blogpost inital design
    
    * improved spacing
    
    * homepage link
    
    * all good
    
    * mod homepage
    
    * mod homepage
    
    * mod layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
 });
-
---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const writingCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    publishedDate: z.date(),
+    tags: z.array(z.string()),
+    title: z.string(),
+    summary: z.string(),
+  }),
+});
+
+export const collections = {
+  writing: writingCollection,
+};
